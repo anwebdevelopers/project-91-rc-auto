b57d3287990df1d9930834c9c66ca23ec807ec8f
Aleksei <anwebdev78@gmail.com>
2020-03-13 16:58:06 +0400

    Верстка главной + преимущества 13.03.2020

--- app/js/main.js
+++ app/js/main.js
@@ -3,10 +3,11 @@
     'use strict';
 
     document.addEventListener( 'DOMContentLoaded', function() {
+
         /*******************************************************/
         //MENU
         /*******************************************************/
-        (function () {
+        (function() {
 
             const elemNav = document.querySelector('.header__menu'),
                 buttonNav = document.querySelector('.header__menu-button');
@@ -20,6 +21,52 @@
 
         }());
 
+        /*******************************************************/
+        //HOME SLIDER
+        /*******************************************************/
+        (function() {
+
+            document.addEventListener('readystatechange', function() {
+
+                if ( document.readyState === 'complete') {
+
+                    $( '.stock' ).each( function () {
+
+                        const $stock = $( this );
+                        $stock.append( '<div class="stock__dots"></div>' )
+                            .find( '.stock__item' )
+                            .addClass( 'swiper-slide' )
+                            .wrapAll( '<div class="stock__container swiper-container"><div class="stock__wrapper swiper-wrapper"></div></div>' );
+
+                        const stockSwiper = new Swiper( $stock.find( '.stock__container' ), {
+
+                            speed: 800,
+                            spaceBetween: 0,
+                            autoHeight: true,
+                            //direction: 'vertical',
+                            loop: true,
+
+                            autoplay: {
+                                delay: 5000,
+                            },
+
+                            pagination: {
+                                el: $stock.find('.stock__dots'),
+                                clickable: true,
+                                type: 'bullets',
+                            },
+
+                        } );
+
+                        window.addEventListener( 'resize', function () {
+                            stockSwiper.updateSize();
+                        } );
+                    } );
+                }
+            });
+
+        }());
+
     });
 
-})();
+}( jQuery ));
